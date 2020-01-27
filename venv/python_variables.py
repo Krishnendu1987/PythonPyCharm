1df7898ad4c9e60ce4a25c54e7252cd9669e06f5
Krishnendu1987 <krishnendu.ke.chowdhury@gmail.com>
2020-01-27 10:47:46 +0530

    Add python list from PyCharm Project

---
+++ venv/python_variables.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+var1 = 'Hello World!'
+var2 = "Python Programming"
+
+print ("var1[0]: ", var1[0])
+print ("var2[1:5]: ", var2[1:5])
+print ("Updated String :- ", var1[:6] + 'Python')
+print ("My name is %s and weight is %d kg!" % ('Zara', 21))
+print ('C:\\nowhere')
+print (r'C:\\nowhere')
+print ('C:\\nowhere')
